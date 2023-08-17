663a04d8e1f2023d1d3379bcb954b9ec41da9e87
Dongwon <davidshin1213@gmail.com>
2023-08-17 18:49:54 +0900

    Main Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <main>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">Home</h1>
     </main>
   );
 }
